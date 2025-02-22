d95e5628e01ba51d8a5330da5c57e75e203d560f
litingyes <luz.liting@gmail.com>
2025-02-22 19:18:10 +0800

    ci(lint/format): use eslint and antfu-config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,11 @@
-import tseslint from '@electron-toolkit/eslint-config-ts'
-import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
-import eslintPluginReact from 'eslint-plugin-react'
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
-import eslintPluginReactRefresh from 'eslint-plugin-react-refresh'
+import antfu from '@antfu/eslint-config'
 
-export default tseslint.config(
-  { ignores: ['**/node_modules', '**/dist', '**/out'] },
-  tseslint.configs.recommended,
-  eslintPluginReact.configs.flat.recommended,
-  eslintPluginReact.configs.flat['jsx-runtime'],
-  {
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    }
+export default antfu({
+  react: true,
+  formatters: true,
+  rules: {
+    'n/prefer-global/process': 0,
+    'no-console': 1,
   },
-  {
-    files: ['**/*.{ts,tsx}'],
-    plugins: {
-      'react-hooks': eslintPluginReactHooks,
-      'react-refresh': eslintPluginReactRefresh
-    },
-    rules: {
-      ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactRefresh.configs.vite.rules
-    }
-  },
-  eslintConfigPrettier
-)
+  ignores: ['./pnpm-lock.yaml'],
+})
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,5 +1,5 @@
-import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge } from 'electron'
 
 // Custom APIs for renderer
 const api = {}
@@ -11,12 +11,15 @@ if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
     contextBridge.exposeInMainWorld('api', api)
-  } catch (error) {
+  }
+  catch (error) {
     console.error(error)
   }
-} else {
-  // @ts-ignore (define in dts)
+}
+else {
+  // @ts-expect-error electron
   window.electron = electronAPI
-  // @ts-ignore (define in dts)
+
+  // @ts-expect-error api
   window.api = api
 }
